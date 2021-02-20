8a2b2bc78f20672939224bc86f722341fd907450
mingyu.ding <15311449479@163.com>
2021-02-20 14:17:42 +0800

    每日练习题

---
+++ 每日练习.py
@@ -0,0 +1,26 @@
+# -*- coding:utf-8 -*-
+# @Author : mingyu.ding
+# @PROJECT: day06
+# @Time : 2021/2/20 14:09
+
+# 用函数实现一个判断用户输入的年份是否是闰年的程序，在函数中打印结果。
+# def year(x):
+#     if (x % 4 == 0 and x % 100 != 0) or x % 400 ==0:
+#         return f"{x}是闰年"
+#     else:
+#         return f"{x}不是闰年"
+#
+# user_year = int(input("请输入年份："))
+# print(year(user_year))
+
+
+
+
+
+def trigon():
+    for i in range(1, 6):
+        print("*"*i)
+
+trigon()
+
+
